db5ea63c1aac61b74add04fa082f56a3709e1c61
Tim <tmeewrnn@gmail.com>
2021-03-21 01:12:17 -0700

    modified test code

--- gen_test_all.py
+++ gen_test_all.py
@@ -7,3 +7,10 @@ def gen_all():
     raw_dir = pathlib.Path('htmlData/raw/')
     raw_csv_path = raw_dir.joinpath('*.csv')
     prep_dir = pathlib.Path('htmlData/preprocessed/')
+    all_files = glob.glob(raw_csv_path.as_posix())
+    for f in all_files:
+        preprocess(str(raw_dir),pathlib.Path(f).name)
+
+if __name__ == '__main__':
+
+    gen_all()
